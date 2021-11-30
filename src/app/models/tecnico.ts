93a3beebc65ae15872fae14aaf2ce823beb5ec7e
marcelorfranca <marcelorfranca@hotmail.com>
2021-11-30 11:39:24 -0300

    Componente Login criado

---
+++ src/app/models/tecnico.ts
@@ -0,0 +1,10 @@
+export interface Tecnico {
+    id?:           any; /** pode ser Integer ou String */
+    nome:       String;
+    cpf:        String;
+    email:      String;
+    senha:      String;
+    perfis:   String[];
+    dataCriacao:   any;
+
+}
