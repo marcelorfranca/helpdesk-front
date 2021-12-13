5babc8b3aa19bd20b51ca1f44a10ef034f37c969
marcelorfranca <marcelorfranca@hotmail.com>
2021-12-13 19:01:19 -0300

    Componente Tecnico Delete criado

--- src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
+++ src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-delete',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoDeleteComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  tecnico: Tecnico = {
+    id:         '',
+    nome:       '',
+    cpf:        '',
+    email:      '',
+    senha:      '',
+    perfis:     [],
+    dataCriacao: ''
   }
 
+  constructor(
+    private service: TecnicoService,
+    private toast: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute, /* ele pega um parametro na URL, no caso o id */
+     ) { }
+
+  ngOnInit(): void { /* componente que se inicia */
+    this.tecnico.id = this.route.snapshot.paramMap.get('id'); /* ele acessa a URL e pega o id - esse id é o mesmo do app-routing*/
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.tecnico.id).subscribe(resposta => {
+      resposta.perfis = []
+      this.tecnico = resposta;
+    })
+  }
+
+  delete(): void {
+    this.service.delete(this.tecnico.id).subscribe(() => {
+      this.toast.success('Técnico deletado com sucesso', 'Deletar');
+      this.router.navigate(['tecnicos'])
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+          console.log(ex);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+        console.log(ex);
+      }
+    })
+  }
+  
 }
