8b98874fa28a0815a6d6c7ea35c4bc26d5602d06
marcelorfranca <marcelorfranca@hotmail.com>
2021-12-28 12:37:16 -0300

    Seviço para atualizar CHAMADO implementado

--- src/app/components/chamado/chamado-update/chamado-update.component.ts
+++ src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from 'src/app/models/chamado';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ChamadoService } from 'src/app/services/chamado.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-chamado-update',
@@ -7,9 +16,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoUpdateComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  chamado: Chamado = {
+    prioridade:        '',
+    status:            '',
+    titulo:            '',
+    observacoes:         '',
+    tecnico:           '',
+    cliente:           '',
+    nomeCliente:       '',
+    nomeTecnico:       '',
   }
 
+  clientes: Cliente[] = []
+  tecnicos: Tecnico[] = []
+
+
+  prioridade: FormControl = new FormControl(null, [Validators.required]);
+  status:     FormControl = new FormControl(null, [Validators.required]);
+  titulo:     FormControl = new FormControl(null, [Validators.required]);
+  observacoes:FormControl = new FormControl(null, [Validators.required]);
+  tecnico:    FormControl = new FormControl(null, [Validators.required]);
+  cliente:    FormControl = new FormControl(null, [Validators.required]);
+
+
+  constructor(
+    private chamadoService: ChamadoService,
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private toastService:    ToastrService, /** vai apresentar mensagem ao usuário */
+    private router:                 Router, /** usado no vai ir para a tela lista após criar registro */
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+    this.findAllClientes();
+    this.findAllTecnicos();
+  }
+
+  findById(): void {
+    this.chamadoService.findById(this.chamado.id).subscribe(resposta => {
+      this.chamado = resposta;
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  update(): void  {
+    this.chamadoService.create(this.chamado).subscribe(resposta => {
+      this.toastService.success('Chamado atualizado com sucesso!', 'Atualiza chamado');
+      this.router.navigate(['chamados'])
+    }, ex => {
+      console.log(ex); //** mostra no console do chrome para ver qual erro ocorreu */
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+
+  findAllClientes(): void {
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta;
+    } )
+  }
+
+  findAllTecnicos(): void {
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.tecnicos = resposta;
+    })
+  }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid && this.status.valid && this.titulo.valid 
+       && this.observacoes.valid && this.tecnico.valid && this.cliente.valid
+  }
+
+  retornaStatus(status: any): string{ //Resolvendo o problema das colunas Prioridade e Status que retorna números.
+    if(status == '0') {
+      return 'ABERTO'
+    } else if(status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string{ //Resolvendo o problema das colunas Prioridade e Status que retorna números.
+    if(prioridade == '0') {
+      return 'BAIXA'
+    } else if(prioridade == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
+  }
+
+ 
+
 }
+
--- src/app/services/chamado.service.ts
+++ src/app/services/chamado.service.ts
@@ -11,6 +11,10 @@ export class ChamadoService {
 
   constructor(private http: HttpClient) { }
   
+  findById(id: any): Observable<Chamado> {
+    return this.http.get<Chamado>(`${API_CONFIG.baseUrl}/chamados/${id}`)
+  }
+
   findAll(): Observable<Chamado[]> { /** metodo que lista todos os chamados */
     return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`);
   }
@@ -20,5 +24,9 @@ export class ChamadoService {
     return this.http.post<Chamado>(`${API_CONFIG.baseUrl}/chamados`, chamado);
   }
     
+  update(chamado: Chamado): Observable<Chamado> {
+    return this.http.put<Chamado>(`${API_CONFIG.baseUrl}/chamados/${chamado.id}`, chamado)
+  }
+
 
 }
